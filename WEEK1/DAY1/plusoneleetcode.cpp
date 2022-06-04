c048947143a310d8b445a9f97fce67e5e65dda84
abhishekdwibedy-2002 <dwibedyabhishek1@gmail.com>
2022-06-05 00:33:42 +0530

    Leetcode Question

--- WEEK1/DAY1/plusoneleetcode.cpp
+++ WEEK1/DAY1/plusoneleetcode.cpp
@@ -8,16 +8,15 @@ public:
     vector<int> plusOne(vector<int>& digits) {
         int l=digits.size();
         for(int i=l-1;i>=0;--i){
-            if(digits[i]+1<9){
-                digits[i]=digits[i]+1-10;
-                if(i==0) 
-                    digits.insert(digits.begin(),1);
+            if(digits[i]<9){
+                digits[i]+=1;
                 return digits;
             } 
-            else
-                digits[i]+=1;
-            break;
-        }
+            else{
+                digits[i]=0;
+            }
+        } 
+        digits.insert(digits.begin(),1);
         return digits;
     }
 };
